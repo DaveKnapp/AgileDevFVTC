e95bb980649113de0ca4df0abec69bd1692180b5
DaveKnapp <knapp3809@fvtc.edu>
2017-02-26 12:12:49 -0600

    added User repo tests

--- T5.Brothership/T5.Brothership.PL.Test/RepositoryIntigration/UserRepositoryTest.cs
+++ T5.Brothership/T5.Brothership.PL.Test/RepositoryIntigration/UserRepositoryTest.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using T5.Brothership.Entities.Models;
 using T5.Brothership.PL.Repositories;
+using System.IO;
+using System.Data.SqlClient;
+using Microsoft.SqlServer.Dts.ManagedConnections;
+
 
 namespace T5.Brothership.PL.Test.RepositoryIntigration
 {
@@ -13,8 +17,20 @@ namespace T5.Brothership.PL.Test.RepositoryIntigration
     [TestClass]
     public class UserRepositoryTest
     {
+        [TestInitialize]
+        public void Initialize()
+        {
+            //TODO Refactor
+            string script = File.ReadAllText(@"../../RepositoryIntigration/AddTestData.sql");
+
+            using (brothershipEntities dataContext = new brothershipEntities())
+            {
+                dataContext.Database.ExecuteSqlCommand(script);
+            }
+
+        }
         [TestMethod]
-        public void Insert_WasRecordInserted_DataExitstInDB()
+        public void Insert_WasRecordInserted_ActualUserNotNull()
         {
             User expectedUser = new User
             {
@@ -31,15 +47,54 @@ namespace T5.Brothership.PL.Test.RepositoryIntigration
             };
 
             User actualUser = new User();
-            using (UserRepository userRepository = new UserRepository())
+            using (UserRepository userRepo = new UserRepository())
             {
-                userRepository.Insert(expectedUser);
-                userRepository.Save();
-                actualUser = userRepository.GetByID(expectedUser);
+                userRepo.Insert(expectedUser);
+                userRepo.Save();
+                actualUser = userRepo.GetByID(expectedUser);
             }
 
             Assert.AreEqual(expectedUser.UserName, actualUser.UserName);
+        }
 
+        [TestMethod]
+        public void GetByID_WasDataGot_ActualUserNotNull()
+        {
+            User expectedUser = new User
+            {
+                ID = 1,
+                UserName = "TestUserOne",
+                Email = "Testing123@yahoo.com",
+                Bio = "This is my bio",
+                ProfileImagePath = "../Images/TestUserOne/profile.png",
+                DateJoined = new DateTime(2017,2,20),
+                DOB = new DateTime(1988,11,12),
+                Gender = "M",
+                UserTypeID = 1,
+                NationalityID = 1
+            };
+
+            User actualUser = new User();
+            using (UserRepository userRepo = new UserRepository())
+            {
+                actualUser = userRepo.GetByID(expectedUser);
+            }
+
+            Assert.AreEqual(expectedUser.ID, actualUser.ID);
+        }
+
+        private void AssertUsersEqual(User expected, User actual)
+        {
+            Assert.AreEqual(expected.ID, actual.ID);
+            Assert.AreEqual(expected.Bio, actual.Bio);
+            Assert.AreEqual(expected.DateJoined, actual.DateJoined);
+            Assert.AreEqual(expected.DOB, actual.DOB);
+            Assert.AreEqual(expected.Email, actual.Email);
+            Assert.AreEqual(expected.Gender, actual.Gender);
+            Assert.AreEqual(expected.NationalityID, actual.NationalityID);
+            Assert.AreEqual(expected.ProfileImagePath, actual.ProfileImagePath);
+            Assert.AreEqual(expected.UserName, actual.UserName);
+            Assert.AreEqual(expected.UserTypeID, actual.UserTypeID);
         }
     }
 }
