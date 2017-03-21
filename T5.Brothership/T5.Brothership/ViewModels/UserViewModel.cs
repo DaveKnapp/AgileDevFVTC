a1859f03ac7cbef4936d373d488be297c7926379
DaveKnapp <knapp3809@fvtc.edu>
2017-03-21 02:37:14 -0500

    Added Password confirm to view

--- T5.Brothership/T5.Brothership/ViewModels/UserViewModel.cs
+++ T5.Brothership/T5.Brothership/ViewModels/UserViewModel.cs
@@ -10,9 +10,16 @@ namespace T5.Brothership.ViewModels
     public class CreateUserViewModel
     {
         public User User { get; set; }
+
         [Required, DataType(DataType.Password),
          StringLength(20, ErrorMessage = "Password must be less than 20 and greater than 5 characters", MinimumLength = 5)]
-        public String Password { get; set; }
+        public string Password { get; set; }
+
+        [Compare(nameof(Password), ErrorMessage = "Passwords don't match "),
+         Display(Name ="Confirm Password"),
+         DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
         public List<Gender> Genders { get; set; }
         public List<Nationality> Nationalities { get; set; }
 
