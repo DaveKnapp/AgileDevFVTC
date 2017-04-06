66061bc6ae323a7f76bad2cf880dbceb219bccd1
DaveKnapp <knapp3809@fvtc.edu>
2017-04-06 09:18:53 -0500

    Started Twitch authorization

---
+++ T5.Brothership/T5.Brothership.BL/TwitchApi/ApiCredentials.cs
@@ -0,0 +1,8 @@
+﻿namespace T5.Brothership.BL.TwitchApi
+{
+    public static class ApiCredentials
+    {
+        public const string CLIENT_ID = "6ivvf370iamtajba1s4tq6fewg9tua";
+        public const string SECRET = "ra16qnvzs0ipifhcczm7egv5kqrvdc";
+    }
+}
---
+++ T5.Brothership/T5.Brothership.BL/TwitchApi/TwitchClient.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T5.Brothership.BL.TwitchApi
+{
+    public class TwitchClient
+    {
+        private readonly HttpClient client = new HttpClient();
+
+        public TwitchClient()
+        {
+            client = CreateClient();
+        }
+
+        public void Dispose()
+        {
+            client.Dispose();
+            GC.SuppressFinalize(this);
+        }
+        public void Authorize(int itemsPerPage, int currentPage)
+        {
+
+            client.BaseAddress = new Uri("https://api.twitch.tv/kraken/games/top/");
+
+        }
+
+        private HttpClient CreateClient()
+        {
+            client.DefaultRequestHeaders.Add("Accept", "application/vnd.twitchtv.v5+json");
+            client.BaseAddress = new Uri("https://api.twitch.tv/kraken/streams/");
+
+            return client;
+        }
+
+        private string CreateRequestString(int itemsPerPage, int currentPage)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(@"?client_id=" + ApiCredentials.CLIENT_ID);
+            builder.Append("&limit=" + itemsPerPage);
+            builder.Append("&offset=" + currentPage * itemsPerPage);
+
+            return builder.ToString();
+        }
+    }
+}
